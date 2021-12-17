06c0b37aac51c174d7719c581771a2818d797674
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-12-17 09:37:03 +0500

    Dynamic Routing With Next Js is so easy

---
+++ Components/Header/index.jsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+
+const Header = () => (
+  <header>
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </li>
+      <li>
+        <Link
+          href="/post/[...slug]"
+          as="/post/1"
+        >
+          <a>First Post</a>
+        </Link>
+      </li>
+      <li>
+        <Link
+          href="/post/[...slug]"
+          as="/post/2"
+        >
+          <a>Second Post</a>
+        </Link>
+      </li>
+    </ul>
+  </header>
+)
+
+export default Header
--- pages/Check/index.jsx
+++ pages/Check/index.jsx
@@ -1,6 +1,9 @@
+import Header from "../../Components/Header";
+
 const Check = () => {
-    return(
+    return (
         <div>
+            <Header />
             <h1 className="text-center mt-2">For dynamic routes in Next JS</h1>
         </div>
     )
