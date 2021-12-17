5405f2f19f2356ce8478eed1157b27312174fed6
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-12-17 09:43:13 +0500

    Dynamic Routing With Next Js is so easy

--- Components/Header/index.jsx
+++ Components/Header/index.jsx
@@ -1,36 +1,44 @@
 import Link from 'next/link'
 
 const Header = () => (
-  <header>
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </li>
-      <li>
-        <Link
-          href="/post/[...slug]"
-          as="/post/1"
-        >
-          <a>First Post</a>
-        </Link>
-      </li>
-      <li>
-        <Link
-          href="/post/[...slug]"
-          as="/post/2"
-        >
-          <a>Second Post</a>
-        </Link>
-      </li>
-    </ul>
-  </header>
+    <header>
+        <ul>
+            <li>
+                <Link href="/">
+                    <a>Home</a>
+                </Link>
+            </li>
+            <li>
+                <Link href="/about">
+                    <a>About</a>
+                </Link>
+            </li>
+            <li>
+                <Link
+                    href="/post/[...slug]"
+                    as="/post/check"
+                >
+                    <a>Check Page</a>
+                </Link>
+            </li>
+            <li>
+                <Link
+                    href="/post/[...slug]"
+                    as="/post/1"
+                >
+                    <a>First Post</a>
+                </Link>
+            </li>
+            <li>
+                <Link
+                    href="/post/[...slug]"
+                    as="/post/2"
+                >
+                    <a>Second Post</a>
+                </Link>
+            </li>
+        </ul>
+    </header>
 )
 
 export default Header
--- pages/post/[...slug].jsx
+++ pages/post/[...slug].jsx
@@ -5,6 +5,8 @@ const Comment = () => {
   const router = useRouter()
   const slug = router.query.slug || []
 
+  
+
   return (
     <>
       <Header />
